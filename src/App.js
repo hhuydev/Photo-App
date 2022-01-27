95a40eedd259f5337ddc76f418083930a0083a4d
hhuydev <hahuy.dev@gmail.com>
2022-01-27 22:13:10 +0700

    login with firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import productApi from "api/productApi";
+import SignIn from "features/Auth/pages/SignIn";
+import firebase from "firebase";
 import React, { Suspense, useEffect, useState } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { Button } from "reactstrap";
 import "./App.scss";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound/index";
 
 const Photo = React.lazy(() => import("./features/Photo"));
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+};
+firebase.initializeApp(config);
 function App() {
   const [productList, setProductList] = useState([]);
 
@@ -27,18 +36,37 @@ function App() {
     fetchProductList();
   }, []);
 
-  console.log(productList);
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(async (user) => {
+        if (!user) {
+          // user logs out, handle something here
+          console.log("User is not logged in");
+          return;
+        }
+
+        console.log("Logged in user: ", user.displayName);
+
+        const token = await user.getIdToken();
+        console.log("Logged in user token: ", token);
+      });
+
+    return () => unregisterAuthObserver();
+  }, []);
+
+  // console.log(productList);
 
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loading ...</div>}>
         <BrowserRouter>
           <Header />
-
           <Switch>
             <Redirect exact from="/" to="/photos" />
 
             <Route path="/photos" component={Photo} />
+            <Route path="/sign-in" component={SignIn} />
             <Route component={NotFound} />
           </Switch>
         </BrowserRouter>
